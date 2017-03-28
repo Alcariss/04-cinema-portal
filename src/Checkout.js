842febf76d1058ae56ae689bbcbb091a3e015c70
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2017-03-29 01:35:37 +0200

    todo task in checkout added

--- src/Checkout.js
+++ src/Checkout.js
@@ -10,10 +10,33 @@ import {
 } from 'semantic-ui-react';
 import PayButton from './buttons/PayButton';
 import Order from './Order';
+import OrdersService from './service/OrdersService';
 
 class Checkout extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      orders: []
+    };
+  }
+
+  /*
+    TODO:
+
+    When this component is rendered, use OrderService to get all orders
+    and render them
+
+    Would be also nice to show loading while orders are being fetched,
+    hint: http://react.semantic-ui.com/elements/loader#loader-example-text
+   */
+
   handlePay () {
-    console.log('TODO');
+    OrdersService.clear(() => {
+      console.log('TODO');
+      // TODO: redirect to Homepage
+    })
   }
 
   renderOrder (order) {
